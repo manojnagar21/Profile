830135d254a5588c3f1828f555a480b195512df7
Mritunjay <mritunjay.sdg@gmail.com>
2024-05-21 20:51:07 +0530

    removed unused port defined in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express, { Application } from 'express';
 import routes from './routes';
 
 const app: Application = express();
-const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use('/api', routes);
