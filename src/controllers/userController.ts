97b547a5046b7c8869aa417266db34bd84de979f
Mritunjay <mritunjay.sdg@gmail.com>
2024-05-27 16:46:30 +0530

    removed password field from get user by id and all users

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -30,7 +30,7 @@ userController.post('/', async (req: Request, res: Response) => {
 
         const savedUser = await userRepository.save(user);
         delete (savedUser as {password?: string}).password;
-        await setCachedUser(savedUser.id.toString(), JSON.stringify(savedUser));
+        await setCachedUser(savedUser._id.toString(), JSON.stringify(savedUser));
         res.status(201).json(savedUser);
     } catch (error) {
         if (error instanceof z.ZodError) {
@@ -52,10 +52,15 @@ userController.get('/:id', async (req: Request, res: Response) => {
             return res.json(JSON.parse(cachedUser));
         }
 
-        const user = await userRepository.findOneBy({ _id: new ObjectId(id) });
+        // const user = await userRepository.findOneBy({ _id: new ObjectId(id) });
+        
+        const user = await userRepository.findOne({
+            where: { _id: new ObjectId(id) },
+            select: ["_id", "name", "email"] // Exclude the password field
+        });
 
         if (user) {
-            await setCachedUser(user.id.toString(), JSON.stringify(user));
+            await setCachedUser(user._id.toString(), JSON.stringify(user));
             return res.json(user);
         }
 
@@ -78,7 +83,10 @@ userController.get('/', async (req: Request, res: Response) => {
             return res.json(JSON.parse(cachedUsers));
         }
 
-        const users = await userRepository.find();
+        // const users = await userRepository.find();
+        const users = await userRepository.find({
+            select: ["_id", "name", "email"] // Exclude the password field
+        });
 
         await setCachedUsers(JSON.stringify(users));
         res.json(users);
